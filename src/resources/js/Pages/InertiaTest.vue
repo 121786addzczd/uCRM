d67f13d2286840be41cb717c44efcedbcc56b5d3
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-05 17:56:32 +0900

    feat: Laravel Breeze(Inertia)環境構築

---
+++ src/resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
+<template>
+inertiaテスト<br>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Link経由です</Link>
+</template>
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: 'http://localhost:5173/',
+    server: {
+        host: true,
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
